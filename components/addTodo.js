f9dde2a4942158531be959e374d8763ce2f55cec
wei30172 <claire610323@gmail.com>
2021-11-02 20:42:25 -0700

    upload

---
+++ components/addTodo.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
+
+export default function AddTodo ({ submitHandler }) {
+  const [text, setText] = useState('')
+  const changeHandler = (val) => {
+    setText(val)
+  }
+  const clickSubmitBotton = (val) => {
+    submitHandler(val);
+  }
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        placeholder='new todo...'
+        onChangeText={changeHandler}
+        value={text}
+      />
+      <Button color='coral' onPress={() => clickSubmitBotton(text)} title='add todo'/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  input: {
+    marginBottom: 10,
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd'
+  }
+})
---
+++ components/sandBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function SandBox() {
+    return (
+        <View style={styles.container}>
+        
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 2,
+    paddingTop: 40,
+    backgroundColor: '#ddd'
+  }
+})
